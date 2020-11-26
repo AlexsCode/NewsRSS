21b064dbc6cf5ea1cc154c289f47c9e30af13297
AlexsCode <alex.tuddenham.at@googlemail.com>
2020-11-26 09:43:44 +0000

    updated bash

--- scripts/News5.sh
+++ scripts/News5.sh
@@ -1,15 +1,12 @@
 #! /bin/bash
-while true; do
-cd /home/pi/NewsRSS/assets
-curl http://feeds.bbci.co.uk/news/rss.xml > /home/pi/NewsRSS/assets/example.txt
-cd /home/pi/NewsRSS/assets/
-./parsetest 
-#echo "latest rss" > /home/pi/NewsRSS/o.txt
-cd /home/pi/NewsRSS/build/
-./NewsRSS &
-sleep 1800
-pkill NewsRSS
-#echo "done"> /home/pi/NewsRSS/o.txt
+while true; do #Starts a constant while loop.
+cd /home/pi/NewsRSS/assets # moves to the correct/assets directory.
+curl http://feeds.bbci.co.uk/news/rss.xml > /home/pi/NewsRSS/assets/example.txt #curl saves from url into text file
+./parsetest #runs the parser to shift through .txt file to look for headlines
+cd /home/pi/NewsRSS/build/ #moves to the build directory
+./NewsRSS & #launches the GUI as a background process
+sleep 1800 #creates a 30 minute pause in the loop-allows for gui to display all headlines
+pkill NewsRSS #kills the GUI process and prepares loop for re-entry
 
-done
-echo "exit"
+done #end loop
+echo "exit" #shows loop has exited. this shouldn't be reached.
